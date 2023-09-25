ce0b55282ace2f613d291f44cbd2d224bbc6c955
Clément Bartholomé <clementoss@gmail.com>
2023-09-25 18:45:05 +0200

    revert changes

--- src/views/BoxScoreView.vue
+++ src/views/BoxScoreView.vue
@@ -10,7 +10,7 @@
 import BoxScore from '../components/BoxScore.vue'
 import AppHeader from '../components/AppHeader.vue'
 import { getSingleBoxScore } from '../util/NbaUtil'
-
+import { useGamesStore } from '../stores/GamesStore'
 export default {
   components: {
     BoxScore,
@@ -19,6 +19,33 @@ export default {
   computed: {
     gameId() {
       return this.$route.params.gameId
+    },
+    homeTeamName() {
+      return this.$route.params.homeTeamName
+    },
+    visitorTeamName() {
+      return this.$route.params.visitorTeamName
+    },
+    gamesBoxScores() {
+      return useGamesStore().gamesBoxScores
+    },
+    playerStats() {
+      if (this.gamesBoxScores && this.gamesBoxScores.length > 0) {
+        const singleBoxScore = getSingleBoxScore(
+          this.gamesBoxScores,
+          this.homeTeamName,
+          this.visitorTeamName
+        )
+        return singleBoxScore[0].PlayerGames
+      }
+      return []
+    }
+  },
+  methods: {
+    async fetchData() {
+      console.log(this.gamesBoxScores)
+      console.log(this.homeTeamName)
+      console.log(this.visitorTeamName)
     }
   }
 }
