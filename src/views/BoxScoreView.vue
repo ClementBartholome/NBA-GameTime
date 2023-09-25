56b1732cf0da5c84dd0beaf4e6670fec986845a1
Clément Bartholomé <clementoss@gmail.com>
2023-09-25 18:20:07 +0200

    Revert "setup BoxScoreView + BoxScore component"
    This reverts commit 18ef6b8a5a57c99b1c34216f44b9c0912c649579.

--- src/views/BoxScoreView.vue
+++ src/views/BoxScoreView.vue
@@ -1,7 +1,7 @@
 <template>
   <AppHeader />
   <main>
-    <h1 @click="fetchData()">Box Score for the game {{ gameId }}</h1>
+    <h1>Box Score for the game {{ gameId }}</h1>
     <BoxScore :playerStats="playerStats" />
   </main>
 </template>
@@ -10,7 +10,6 @@
 import BoxScore from '../components/BoxScore.vue'
 import AppHeader from '../components/AppHeader.vue'
 import { getSingleBoxScore } from '../util/NbaUtil'
-import { useGamesStore } from '../stores/GamesStore'
 
 export default {
   components: {
@@ -20,33 +19,6 @@ export default {
   computed: {
     gameId() {
       return this.$route.params.gameId
-    },
-    homeTeamName() {
-      return this.$route.params.homeTeamName
-    },
-    visitorTeamName() {
-      return this.$route.params.visitorTeamName
-    },
-    gamesBoxScores() {
-      return useGamesStore().gamesBoxScores
-    },
-    playerStats() {
-      if (this.gamesBoxScores && this.gamesBoxScores.length > 0) {
-        const singleBoxScore = getSingleBoxScore(
-          this.gamesBoxScores,
-          this.homeTeamName,
-          this.visitorTeamName
-        )
-        return singleBoxScore[0].PlayerGames
-      }
-      return []
-    }
-  },
-  methods: {
-    async fetchData() {
-      console.log(this.gamesBoxScores)
-      console.log(this.homeTeamName)
-      console.log(this.visitorTeamName)
     }
   }
 }
