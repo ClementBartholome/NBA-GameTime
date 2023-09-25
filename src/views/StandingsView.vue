cccdab65677b72755f46427b78c84416f6fd3837
Clément Bartholomé <clementoss@gmail.com>
2023-09-25 18:45:38 +0200

    remove missing seasons in api

--- src/views/StandingsView.vue
+++ src/views/StandingsView.vue
@@ -67,9 +67,7 @@ interface TeamStandings {
 const standings = ref<TeamStandings[]>([])
 const selectedSeason = ref<number>(2023) // Default selected season
 const availableSeasons = ref<number[]>([
-  2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008,
-  2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995, 1994, 1993, 1992,
-  1991, 1990, 1989, 1988
+  2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009
 ])
 
 const teamLogos = ref<{ [key: string]: string }>({}) // Store team logos with team names as keys
