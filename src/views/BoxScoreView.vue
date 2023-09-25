feb98293ca92fa12eab3ee1177b625005654768b
Clément Bartholomé <clementoss@gmail.com>
2023-09-25 18:40:39 +0200

    Revert "setup BoxScoreView + BoxScore component"
    This reverts commit 18ef6b8a5a57c99b1c34216f44b9c0912c649579.

--- src/views/BoxScoreView.vue
+++ src/views/BoxScoreView.vue
@@ -5,21 +5,3 @@
     <BoxScore :playerStats="playerStats" />
   </main>
 </template>
-
-<script lang="ts">
-import BoxScore from '../components/BoxScore.vue'
-import AppHeader from '../components/AppHeader.vue'
-import { getSingleBoxScore } from '../util/NbaUtil'
-
-export default {
-  components: {
-    BoxScore,
-    AppHeader
-  },
-  computed: {
-    gameId() {
-      return this.$route.params.gameId
-    }
-  }
-}
-</script>
