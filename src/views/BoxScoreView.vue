9293b5208f53d3aae4a48e6f19ee2b998cab6fb5
Clément Bartholomé <clementoss@gmail.com>
2023-09-25 18:44:57 +0200

    revert changes

--- src/views/BoxScoreView.vue
+++ src/views/BoxScoreView.vue
@@ -5,3 +5,21 @@
     <BoxScore :playerStats="playerStats" />
   </main>
 </template>
+
+<script lang="ts">
+import BoxScore from '../components/BoxScore.vue'
+import AppHeader from '../components/AppHeader.vue'
+import { getSingleBoxScore } from '../util/NbaUtil'
+
+export default {
+  components: {
+    BoxScore,
+    AppHeader
+  },
+  computed: {
+    gameId() {
+      return this.$route.params.gameId
+    }
+  }
+}
+</script>
