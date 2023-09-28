bd23912490dabd49ada3f4d6d92a15a3d40a662f
Clément Bartholomé <clementoss@gmail.com>
2023-09-28 11:16:06 +0200

    style: boxscore responsive

--- src/components/BoxScore.vue
+++ src/components/BoxScore.vue
@@ -2,53 +2,57 @@
   <div class="box-score">
     <div class="team">
       <h2>{{ homeTeamName }}</h2>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Player</th>
-            <th>PTS</th>
-            <th>REB</th>
-            <th>AST</th>
-            <th>MIN</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(player, index) in sortedHomeTeamPlayers" :key="index">
-            <td>{{ player.Started ? 'Starter' : 'Bench' }}</td>
-            <td>{{ player.Name }}</td>
-            <td>{{ player.Points }}</td>
-            <td>{{ player.Rebounds }}</td>
-            <td>{{ player.Assists }}</td>
-            <td>{{ player.Minutes }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Player</th>
+              <th>PTS</th>
+              <th>REB</th>
+              <th>AST</th>
+              <th>MIN</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(player, index) in sortedHomeTeamPlayers" :key="index">
+              <td>{{ player.Started ? 'Starter' : 'Bench' }}</td>
+              <td>{{ player.Name }}</td>
+              <td>{{ player.Points }}</td>
+              <td>{{ player.Rebounds }}</td>
+              <td>{{ player.Assists }}</td>
+              <td>{{ player.Minutes }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
     <div class="team">
       <h2>{{ visitorTeamName }}</h2>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Player</th>
-            <th>PTS</th>
-            <th>REB</th>
-            <th>AST</th>
-            <th>MIN</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(player, index) in sortedVisitorTeamPlayers" :key="index">
-            <td>{{ player.Started ? 'Starter' : 'Bench' }}</td>
-            <td>{{ player.Name }}</td>
-            <td>{{ player.Points }}</td>
-            <td>{{ player.Rebounds }}</td>
-            <td>{{ player.Assists }}</td>
-            <td>{{ player.Minutes }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Player</th>
+              <th>PTS</th>
+              <th>REB</th>
+              <th>AST</th>
+              <th>MIN</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(player, index) in sortedVisitorTeamPlayers" :key="index">
+              <td>{{ player.Started ? 'Starter' : 'Bench' }}</td>
+              <td>{{ player.Name }}</td>
+              <td>{{ player.Points }}</td>
+              <td>{{ player.Rebounds }}</td>
+              <td>{{ player.Assists }}</td>
+              <td>{{ player.Minutes }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -110,7 +114,6 @@ export default {
   background-color: #fff;
   padding: 20px;
   border: 1px solid #ddd;
-  margin: 20px;
   color: #333;
   font-family: Arial, sans-serif;
 }
@@ -130,6 +133,11 @@ export default {
 }
 
 /* Table Styling */
+.table-container {
+  width: 100%;
+  overflow-x: auto;
+  margin-top: 10px;
+}
 table {
   width: 100%;
   border-collapse: collapse;
@@ -153,5 +161,21 @@ td:empty::before {
   content: 'DNP';
   font-weight: bold;
   color: #999;
+}
+
+@media (max-width: 1024px) {
+  .box-score {
+    flex-direction: column;
+  }
+
+  .team {
+    margin-bottom: 20px;
+  }
+}
+
+@media (max-width: 768px) {
+  .box-score {
+    padding: 0;
+  }
 }
 </style>
