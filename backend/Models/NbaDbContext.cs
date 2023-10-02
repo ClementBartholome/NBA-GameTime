2dc15bb1884d0ac45916c2dd824aa827136d3ac2
Clément Bartholomé <clementoss@gmail.com>
2023-10-02 09:23:24 +0200

    comments

--- backend/Models/NbaDbContext.cs
+++ backend/Models/NbaDbContext.cs
@@ -4,16 +4,17 @@ namespace backend.Models
 {
     public class NbaDbContext : DbContext
     {
+        // Constructor that takes DbContextOptions as a parameter.
         public NbaDbContext(DbContextOptions<NbaDbContext> options) : base(options)
         {
+            // Initialize the context 
         }
 
+        // Allows querying and interacting with the 'Games' table in the database.
         public DbSet<Game> Games { get; set; }
         public DbSet<Team> Teams { get; set; }
         public DbSet<Standings> Standings { get; set; }
         public DbSet<Player> Players { get; set; }
         public DbSet<PlayerSeasonAverages> PlayerSeasonAverages { get; set; }
-
-      
     }
 }
